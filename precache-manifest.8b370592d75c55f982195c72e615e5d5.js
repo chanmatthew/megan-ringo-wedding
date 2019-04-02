36214e7008c2a83ab26cc6ae2a17622aa6f01466
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2019-04-01 20:27:37 -0400

    Updates

---
+++ precache-manifest.8b370592d75c55f982195c72e615e5d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/megan-ringo-wedding/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2a476e08104fb333e5f7",
+    "url": "/megan-ringo-wedding/static/js/runtime~main.0d5003ee.js"
+  },
+  {
+    "revision": "89226abafc5465fef693",
+    "url": "/megan-ringo-wedding/static/js/main.f2bc0b95.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/megan-ringo-wedding/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "89226abafc5465fef693",
+    "url": "/megan-ringo-wedding/static/css/main.8136b8f3.chunk.css"
+  },
+  {
+    "revision": "c6a7abb76592c143d17eff6654b73c8f",
+    "url": "/megan-ringo-wedding/index.html"
+  }
+];
