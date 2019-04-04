15b0113ffb56716dc7a4f452b1d421ffbf88fe21
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2019-04-03 23:11:25 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/megan-ringo-wedding/precache-manifest.8b370592d75c55f982195c72e615e5d5.js"
+  "/precache-manifest.384839ccef36e371dda515329ac66cd1.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/megan-ringo-wedding/index.html", {
+workbox.routing.registerNavigationRoute("/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
