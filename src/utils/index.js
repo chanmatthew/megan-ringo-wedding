629d754d5f51daa33be84c288235e177afb5404a
Ken Chan <thiskenchan@gmail.com>
2019-05-17 03:45:12 -0400

    added google calendar url to redirect user into google calendar and prompting them with a ui for adding the event to their calendar, refactored event data into single object for ease of modification, added date enums and utils
    added time zone abbr to time section

---
+++ src/utils/index.js
@@ -0,0 +1,15 @@
+import {
+  formatDate,
+  formatHours,
+  getCalendarIsoString,
+  getMeridiem,
+  getTimeZoneAbbr
+} from "./date";
+
+export {
+  formatDate,
+  formatHours,
+  getCalendarIsoString,
+  getMeridiem,
+  getTimeZoneAbbr
+};
