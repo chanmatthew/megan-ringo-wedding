0dbe9bc63acf35eb226e8610982ad321bf8beee8
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2019-08-06 13:45:25 -0400

    Updated homepage image and added mobile responsive media queries

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -2,7 +2,20 @@ import React, { useEffect } from "react";
 import styled from "@emotion/styled/macro";
 
 import { AppContext } from "../AppProvider";
-import CameraOverlay from "../components/CameraOverlay";
+import { MIN_WIDTH_BREAKPOINTS } from "../enums";
+
+const [
+  ,
+  ,
+  POST_IPHONE6_PORTRAIT_UP,
+  ,
+  PHONE_LANDSCAPE_UP,
+  ,
+  BETWEEN_SMALL_DEVICES_TABLET_UP,
+  TABLET_PORTRAIT_UP,
+  TABLET_LANDSCAPE_UP,
+  DESKTOP_UP
+] = MIN_WIDTH_BREAKPOINTS;
 
 const StyledMain = styled.main`
   position: absolute;
@@ -16,41 +29,102 @@ const StyledHero = styled.div`
   position: relative;
   width: 100%;
   height: 100vh;
-  min-height: 67.5em;
-  background-image: url(${props => props.src || "none"});
-  background-position: center;
+  background-image: url(${props => `${props.src}_mobile.jpg`}),
+    linear-gradient(to bottom, rgba(27, 38, 42, 1), rgba(84, 84, 92, 1));
+  background-position: center bottom;
   background-repeat: no-repeat;
   background-size: cover;
-  background-color: rgba(21, 37, 64, 1);
+  min-height: 45em;
+
+  @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
+    background-image: url(${props => `${props.src}.jpg`}),
+      linear-gradient(to bottom, rgba(27, 38, 42, 1), rgba(84, 84, 92, 1));
+    min-height: 55em;
+  }
+
+  @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
+    min-height: 65em;
+  }
+
+  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
+    min-height: 75em;
+  }
 `;
 
 const StyledMission = styled.div`
   position: absolute;
-  top: 42%;
-  left: 2.8em;
+  left: 50%;
+  transform: translate3d(-50%, 0, 0);
+  bottom: 65%;
+
+  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
+    bottom: 75%;
+    transform: translate3d(-75%, 0, 0);
+  }
+
+  @media only screen and (min-width: ${DESKTOP_UP}px) {
+    transform: translate3d(-78%, 0, 0);
+  }
 `;
 
 const StyledMissionTitle = styled.p`
-  font-size: 2.25rem;
-  font-weight: bold;
-  color: rgba(255, 255, 255, 1);
-`;
-
-const StyledMissionBody = styled.p`
-  font-size: 5rem;
   font-family: "Futura PT Extra";
+  text-align: center;
+  margin-bottom: 0.1em;
+  font-size: 1.8rem;
+
+  @media only screen and (min-width: ${POST_IPHONE6_PORTRAIT_UP}px) {
+    font-size: 2.1rem;
+  }
+
+  @media only screen and (min-width: ${PHONE_LANDSCAPE_UP}px) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
+    font-size: 2.1rem;
+  }
+
+  @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
+    font-size: 2.5rem;
+  }
+
+  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
+    font-size: 4rem;
+  }
+
+  @media only screen and (min-width: ${DESKTOP_UP}px) {
+    font-size: 4.6rem;
+  }
 `;
 
 const StyledColoredSpan = styled.span`
   color: ${props => props.color};
 `;
 
-const StyledMissionFooter = styled.p`
-  font-size: 1.5rem;
+const StyledMissionSubtitle = styled.p`
   font-family: "Futura PT Demi";
   color: rgba(255, 255, 255, 1);
-  text-align: right;
   letter-spacing: 0.05em;
+  text-align: center;
+  font-size: 0.875rem;
+
+  @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
+    font-size: 1rem;
+  }
+
+  @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
+    font-size: 1.2rem;
+  }
+
+  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
+    font-size: 1.6rem;
+    text-align: right;
+  }
+
+  @media only screen and (min-width: ${DESKTOP_UP}px) {
+    font-size: 2rem;
+  }
 `;
 
 const Homepage = ({ handleIsBrandDark, handleIsNavbarDark }) => {
@@ -61,21 +135,19 @@ const Homepage = ({ handleIsBrandDark, handleIsNavbarDark }) => {
 
   return (
     <StyledMain>
-      <StyledHero src="/img/photos/bridge_in_forest.jpg">
-        <CameraOverlay />
+      <StyledHero src="/img/photos/homepage_photo">
         <StyledMission>
-          <StyledMissionTitle>WE'RE GETTING MARRIED!</StyledMissionTitle>
-          <StyledMissionBody>
+          <StyledMissionTitle>
             <StyledColoredSpan color="rgba(128, 149, 184, 1)">
               #
             </StyledColoredSpan>
             <StyledColoredSpan color="rgba(246, 233, 203, 1)">
               MINGOWEDDING
             </StyledColoredSpan>
-          </StyledMissionBody>
-          <StyledMissionFooter>
+          </StyledMissionTitle>
+          <StyledMissionSubtitle>
             10&#183;16&#183;19 | NEW YORK
-          </StyledMissionFooter>
+          </StyledMissionSubtitle>
         </StyledMission>
       </StyledHero>
     </StyledMain>
