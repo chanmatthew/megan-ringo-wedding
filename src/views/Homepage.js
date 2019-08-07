680d210461876528528ebb74542f65999c15f3a1
Matthew Chan <matthewchan@Matthews-MacBook-Pro.local>
2019-08-07 16:55:11 -0400

    Separated image src props for desktop and mobile and swapped the mission to an svg

--- src/views/Homepage.js
+++ src/views/Homepage.js
@@ -9,7 +9,7 @@ const [
   ,
   POST_IPHONE6_PORTRAIT_UP,
   ,
-  PHONE_LANDSCAPE_UP,
+  ,
   ,
   BETWEEN_SMALL_DEVICES_TABLET_UP,
   TABLET_PORTRAIT_UP,
@@ -29,101 +29,47 @@ const StyledHero = styled.div`
   position: relative;
   width: 100%;
   height: 100vh;
-  background-image: url(${props => `${props.src}_mobile.jpg`}),
+  background-image: url(${props => props.mobileSrc}),
     linear-gradient(to bottom, rgba(27, 38, 42, 1), rgba(84, 84, 92, 1));
   background-position: center bottom;
   background-repeat: no-repeat;
   background-size: cover;
-  min-height: 45em;
+  min-height: 35em;
+
+  @media only screen and (min-width: ${POST_IPHONE6_PORTRAIT_UP}px) {
+    min-height: 45em;
+  }
 
   @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
-    background-image: url(${props => `${props.src}.jpg`}),
+    background-image: url(${props => props.desktopSrc}),
       linear-gradient(to bottom, rgba(27, 38, 42, 1), rgba(84, 84, 92, 1));
-    min-height: 55em;
+    min-height: 45em;
   }
 
   @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
-    min-height: 65em;
+    min-height: 55em;
   }
 
   @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
+    min-height: 65em;
+  }
+
+  @media only screen and (min-width: ${DESKTOP_UP}px) {
     min-height: 75em;
   }
 `;
 
-const StyledMission = styled.div`
+const StyledGraphic = styled.img`
   position: absolute;
-  left: 50%;
-  transform: translate3d(-50%, 0, 0);
-  bottom: 65%;
-
-  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
-    bottom: 75%;
-    transform: translate3d(-75%, 0, 0);
-  }
-
-  @media only screen and (min-width: ${DESKTOP_UP}px) {
-    transform: translate3d(-78%, 0, 0);
-  }
-`;
-
-const StyledMissionTitle = styled.p`
-  font-family: "Futura PT Extra";
-  text-align: center;
-  margin-bottom: 0.1em;
-  font-size: 1.8rem;
-
-  @media only screen and (min-width: ${POST_IPHONE6_PORTRAIT_UP}px) {
-    font-size: 2.1rem;
-  }
-
-  @media only screen and (min-width: ${PHONE_LANDSCAPE_UP}px) {
-    font-size: 2.5rem;
-  }
+  height: 10em;
+  bottom: 55%;
+  left: 0;
+  right: 0;
+  margin: 0 auto;
+  display: block;
 
   @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
-    font-size: 2.1rem;
-  }
-
-  @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
-    font-size: 2.5rem;
-  }
-
-  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
-    font-size: 4rem;
-  }
-
-  @media only screen and (min-width: ${DESKTOP_UP}px) {
-    font-size: 4.6rem;
-  }
-`;
-
-const StyledColoredSpan = styled.span`
-  color: ${props => props.color};
-`;
-
-const StyledMissionSubtitle = styled.p`
-  font-family: "Futura PT Demi";
-  color: rgba(255, 255, 255, 1);
-  letter-spacing: 0.05em;
-  text-align: center;
-  font-size: 0.875rem;
-
-  @media only screen and (min-width: ${BETWEEN_SMALL_DEVICES_TABLET_UP}px) {
-    font-size: 1rem;
-  }
-
-  @media only screen and (min-width: ${TABLET_PORTRAIT_UP}px) {
-    font-size: 1.2rem;
-  }
-
-  @media only screen and (min-width: ${TABLET_LANDSCAPE_UP}px) {
-    font-size: 1.6rem;
-    text-align: right;
-  }
-
-  @media only screen and (min-width: ${DESKTOP_UP}px) {
-    font-size: 2rem;
+    display: none;
   }
 `;
 
@@ -135,20 +81,11 @@ const Homepage = ({ handleIsBrandDark, handleIsNavbarDark }) => {
 
   return (
     <StyledMain>
-      <StyledHero src="/img/photos/homepage_photo">
-        <StyledMission>
-          <StyledMissionTitle>
-            <StyledColoredSpan color="rgba(128, 149, 184, 1)">
-              #
-            </StyledColoredSpan>
-            <StyledColoredSpan color="rgba(246, 233, 203, 1)">
-              MINGOWEDDING
-            </StyledColoredSpan>
-          </StyledMissionTitle>
-          <StyledMissionSubtitle>
-            10&#183;16&#183;19 | NEW YORK
-          </StyledMissionSubtitle>
-        </StyledMission>
+      <StyledHero
+        desktopSrc="/img/photos/homepage_photo.jpg"
+        mobileSrc="/img/photos/homepage_photo_mobile.jpg"
+      >
+        <StyledGraphic src="/img/graphics/homepage_graphic.svg" />
       </StyledHero>
     </StyledMain>
   );
