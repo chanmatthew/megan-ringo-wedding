acfb2915257e5936b6277207f8fc77633dd1b861
Matthew Chan <matthewchan17@gmail.com>
2019-08-13 00:19:09 +0000

    Updates

---
+++ precache-manifest.28d354a869b99e76858f643e4e7ab337.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "3f9fd8398c9f4054e408",
+    "url": "/static/js/6.9f795e72.chunk.js"
+  },
+  {
+    "revision": "c64bf544e6485c987f9b",
+    "url": "/static/js/0.b52195de.chunk.js"
+  },
+  {
+    "revision": "b1528e2eda41448e3e65",
+    "url": "/static/js/main.15365180.chunk.js"
+  },
+  {
+    "revision": "772dce149c736c2e5f49",
+    "url": "/static/js/runtime~main.023f2b01.js"
+  },
+  {
+    "revision": "9b93b4565adf3f0cf39b",
+    "url": "/static/js/3.e19fe05a.chunk.js"
+  },
+  {
+    "revision": "bfb1c80a945887db55e2",
+    "url": "/static/js/4.3b25ef22.chunk.js"
+  },
+  {
+    "revision": "da25363aaf62669c7526",
+    "url": "/static/js/5.08010fcb.chunk.js"
+  },
+  {
+    "revision": "565cfcfd03bdaa0f6e70",
+    "url": "/static/js/12.adee06a9.chunk.js"
+  },
+  {
+    "revision": "6753864d77c013f480c5",
+    "url": "/static/js/7.7cc6452a.chunk.js"
+  },
+  {
+    "revision": "f50f768540b7ee830fbc",
+    "url": "/static/js/8.a668c54d.chunk.js"
+  },
+  {
+    "revision": "6f1216d04d6b922b0e8a",
+    "url": "/static/js/9.2f93756b.chunk.js"
+  },
+  {
+    "revision": "1eb36645a37bb2c58edf",
+    "url": "/static/js/10.eb31885e.chunk.js"
+  },
+  {
+    "revision": "3acdb76f66de091e48b0",
+    "url": "/static/js/11.2868f0a8.chunk.js"
+  },
+  {
+    "revision": "b1528e2eda41448e3e65",
+    "url": "/static/css/main.9f0dc64f.chunk.css"
+  },
+  {
+    "revision": "b8ae9f97d00a9a52e6e8135787aa68fa",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1bc2852dc6d1072a55c1e3898e53ba19.js"
+  "/precache-manifest.28d354a869b99e76858f643e4e7ab337.js"
 );
 
 workbox.clientsClaim();
